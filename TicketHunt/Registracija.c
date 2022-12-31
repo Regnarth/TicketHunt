f5fbd04d24a2a979c6f15c87b0d6d77257b06b9c
David Đenadija <59781575+Regnarth@users.noreply.github.com>
2022-12-31 15:06:16 +0100

    Update Registracija.c

--- TicketHunt/Registracija.c
+++ TicketHunt/Registracija.c
@@ -2,11 +2,33 @@
 #include <string.h>
 #include "Registracija.h"
 
-void register_user(struct User* user) {
-  printf("Enter a username: ");
-  fgets(user->username, MAX_USERNAME_LENGTH, stdin);
-  printf("Enter a password: ");
-  fgets(user->password, MAX_PASSWORD_LENGTH, stdin);
+void register_user()
+{
+    struct User user;
+    FILE* fp;
+    int pom;
+    printf("Enter a username: ");
+    fgets(user.username, MAX_USERNAME_LENGTH, stdin);
+    if(provjeriUsername(user.username))
+    {
+        if(fp=fopen("Korisnici.txt","w"))
+        {
+            fprintf(fp,"%s",user.username);
+            printf("Enter a password: ");
+            fgets(user.password, MAX_PASSWORD_LENGTH, stdin);
+            fprintf(fp,"%s",user.password);
+        }else printf("Neuspjesno otvoren fajl!");
+
+
+    }
 }
 
-
+int provjeriUsername(char* username){
+FILE *fp;
+char pom[MAX_USERNAME_LENGTH];
+if(fp=fopen("Korisnici.txt","w")){
+  fscanf(fp,"%s",pom);
+  if(strcmp(pom,username)==0)return 1;
+} else printf("Neuspjesno otvoren fajl!");
+return 0;
+}
