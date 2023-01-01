b06cd77e3993b29dca2d8985b61c8a8b78567f3a
GlorijaSamardzija <121225674+GlorijaSamardzija@users.noreply.github.com>
2023-01-01 15:54:09 +0100

    Update main.c

--- TicketHunt/main.c
+++ TicketHunt/main.c
@@ -1,11 +1,13 @@
 //#include <stdio.h>
 #include <stdlib.h>
 #include "Registracija.c"
+#include "KreiranjeNaloga.c"
 
 int main()
 {
 struct User user;
   register_user(&user);
   printf("Successfully registered user with username '%s' and password '%s'\n", user.username, user.password);
+  kreirajNalog();
   return 0;
 }
