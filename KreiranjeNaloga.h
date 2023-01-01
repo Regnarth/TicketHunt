42a8937a2e18b249c44ee6a9b6179025caba155f
GlorijaSamardzija <121225674+GlorijaSamardzija@users.noreply.github.com>
2023-01-01 15:57:55 +0100

    Create KreiranjeNaloga.h

---
+++ KreiranjeNaloga.h
@@ -0,0 +1,28 @@
+#define MAX 20
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+typedef struct klijent {
+	char ime[MAX];
+	char prezime[MAX];
+	char korisnickoIme[MAX];
+	char sifra[MAX];
+	int suspendovan;
+} KLIJENT;
+
+typedef struct administrator {
+	char ime[MAX];
+	char prezime[MAX];
+	char korisnickoIme[MAX];
+	char sifra[MAX];
+} ADMINISTRATOR;
+
+void kreirajNalog();
+int dodajKlijenta(FILE*, KLIJENT*);
+int dodajAdministratora(FILE*, ADMINISTRATOR*);
+int traziKlijenta(FILE*, char*);
+int traziAdministratora(FILE*, char*);
+int provjeraFormataSifre(char*);
+void unosKlijenta(KLIJENT*);
+void unosAdministratora(ADMINISTRATOR*);
