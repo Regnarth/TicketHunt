90b277723855deaf940793b3070db9901e4c9b8f
GlorijaSamardzija <121225674+GlorijaSamardzija@users.noreply.github.com>
2023-01-02 21:27:25 +0100

    Add files via upload

---
+++ TicketHunt/SuspendovanjeNaloga.c
@@ -0,0 +1,49 @@
+#pragma once
+#include "SuspendovanjeNaloga.h"
+
+void suspendujNalog() {
+
+	char korisnickoIme[MAX];
+	printf("Unesite korisnicko ime naloga koji zelite da suspendujete.\n");
+	scanf("%s", korisnickoIme);
+	FILE* f = 0;
+	while (provjeraSuspenzije(f, korisnickoIme)) {
+		printf("Izaberite drugi nalog.\n");
+		scanf("%s", korisnickoIme);
+	}
+
+	if (memorisiNalog(f, korisnickoIme))
+		printf("Nalog je uspjesno suspendovan.\n");
+
+}
+
+int provjeraSuspenzije(FILE* f, char* korisnickoIme) {
+
+	if (f = fopen("Suspendovani.txt", "r")) {
+		char pom[MAX];
+		while (fscanf(f, "%s", pom) == 1) {
+			if (strcmp(korisnickoIme, pom) == 0) {
+				printf("Nalog je vec suspendovan.\n");
+				fclose(f);
+				return 1;
+			}
+		}
+	}
+
+	return 0;
+
+}
+
+int memorisiNalog(FILE* f, char* korisnickoIme) {
+
+	if (f = fopen("Suspendovani.txt", "a")) {
+
+		fprintf(f, "%s\n", korisnickoIme);
+		fclose(f);
+		return 1;
+	}
+	else {
+		printf("Neuspjesno otvaranje datoteke.\n");
+		return 0;
+	}
+}
