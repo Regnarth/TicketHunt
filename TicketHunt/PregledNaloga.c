d38d59ca2220d3abced3f1be86b00338733f0b20
David Đenadija <59781575+Regnarth@users.noreply.github.com>
2023-01-03 00:37:53 +0100

    Pregled kreiranih naloga

---
+++ TicketHunt/PregledNaloga.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include "PregledNaloga.h"
+#include "Registracija.h"
+
+void pregledNaloga(){
+FILE* fp;
+if(fp=fopen("Klijenti.txt","r")){
+    USER pom;
+    while(fscanf(fp,"%s %s %s %s",pom.ime,pom.prezime,pom.username,pom.password)!=EOF){
+        printf("%s %s %s %s\n",pom.ime,pom.prezime,pom.username,pom.password);
+    }
+    fclose(fp);
+}
+if(fp=fopen("Administratori.txt","r")){
+    USER pom;
+    while(fscanf(fp,"%s %s %s %s",pom.ime,pom.prezime,pom.username,pom.password)!=EOF){
+        printf("%s %s %s %s\n",pom.ime,pom.prezime,pom.username,pom.password);
+    }
+    fclose(fp);
+}
+}
---
+++ TicketHunt/PregledNaloga.h
@@ -0,0 +1,6 @@
+#ifndef PREGLED_NALOGA_H
+#define PREGLED_NALOGA_H
+ // PREGLED_NALOGA_H
+
+
+#endif
