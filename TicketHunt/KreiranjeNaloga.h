77032a92cce292f6a9c93c3656402a11fba694ae
GlorijaSamardzija <121225674+GlorijaSamardzija@users.noreply.github.com>
2023-01-03 10:57:04 +0100

    Update KreiranjeNaloga.h

--- TicketHunt/KreiranjeNaloga.h
+++ TicketHunt/KreiranjeNaloga.h
@@ -1,4 +1,5 @@
-#pragma once
+#ifndef KREIRANJENALOGA_H
+#define KREIRANJENALOGA_H
 #define MAX 20
 #include <stdio.h>
 #include <string.h>
@@ -18,13 +19,5 @@ typedef struct administrator {
 	char korisnickoIme[MAX];
 	char sifra[MAX];
 } ADMINISTRATOR;
-
-void kreirajNalog();
-int dodajKlijenta(FILE*, KLIJENT*);
-int dodajAdministratora(FILE*, ADMINISTRATOR*);
-int traziKlijenta(FILE*, char*);
-int traziAdministratora(FILE*, char*);
-int provjeraFormataSifre(char*);
-void unosKlijenta(KLIJENT*);
-void unosAdministratora(ADMINISTRATOR*);
+#endif
 
