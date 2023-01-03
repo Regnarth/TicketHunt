889cd676879aec475461dd664cf018571ac5bf35
MarijaZ5 <120033001+MarijaZ5@users.noreply.github.com>
2023-01-03 20:39:37 +0100

    Add files via upload
    Header fajl za kreiranje dogadjaja

---
+++ KreiranjeDogadjaja.h
@@ -0,0 +1,31 @@
+#ifndef KREIRANJE_DOGADJAJA_H
+#define KREIRANJE_DOGADJAJA_H
+#define MAX 20
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+struct Datum
+{
+    int dan;
+    int mjesec;
+    int godina;
+};
+
+struct Vrijeme_odrzavanja
+{
+    int sat;
+    int minut;
+};
+typedef struct dogadjaj {
+	char naziv[MAX];
+	struct Datum datum;
+	char mjesto[MAX];
+	struct Vrijeme_odrzavanja vrijeme;
+	int cijena_ulaznice;
+	int broj_mjesta;
+	int broj_prodatih_ulaznica;
+} DOGADJAJ;
+
+
+
+#endif
