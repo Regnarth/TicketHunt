37836db0cc4d28c0ae3bfbc41961e55274bae675
mihajloseva <mihajlo.seva@student.etf.unibl.org>
2023-01-05 14:27:10 +0100

    Update PregledDogadjaja - dodano mjesto dogadjaja

--- TicketHunt/PregledDogadjaja.c
+++ TicketHunt/PregledDogadjaja.c
@@ -8,12 +8,12 @@ void PregledDogadjaja()
     {
         DOGADJAJ temp;
         printf("DOGADJAJI:\n");
-        while (fscanf(fp, "%s %d.%d.%d %d:%d %d %d %d\n", temp.naziv, &temp.datum.dan,
-            &temp.datum.mjesec, &temp.datum.godina, &temp.vrijeme.sat, &temp.vrijeme.minut,
-            &temp.cijena_ulaznice, &temp.broj_mjesta, &temp.broj_prodatih_ulaznica) != EOF)
+        while (  fscanf(fp, "%s %d.%d.%d %d:%d %s %d %d %d\n", temp.naziv, &temp.datum.dan,
+                 &temp.datum.mjesec, &temp.datum.godina, &temp.vrijeme.sat, &temp.vrijeme.minut, temp.mjesto,
+                 &temp.cijena_ulaznice, &temp.broj_mjesta, &temp.broj_prodatih_ulaznica)   !=  EOF )
 
-            printf("%s %d.%d.%d %d:%d %d %d %d\n", temp.naziv, temp.datum.dan,
-                temp.datum.mjesec, temp.datum.godina, temp.vrijeme.sat, temp.vrijeme.minut,
+            printf("%s %d.%d.%d %d:%d %s %d %d %d\n", temp.naziv, temp.datum.dan,
+                temp.datum.mjesec, temp.datum.godina, temp.vrijeme.sat, temp.vrijeme.minut, temp.mjesto,
                 temp.cijena_ulaznice, temp.broj_mjesta, temp.broj_prodatih_ulaznica);
 
         fclose(fp);
