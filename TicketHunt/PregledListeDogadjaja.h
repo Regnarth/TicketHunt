3b4618d766dd31bd1c51ba0477e2f0e588e8ef2d
DanicaG2 <121254737+DanicaG2@users.noreply.github.com>
2023-01-08 11:16:13 +0100

    Add files via upload

---
+++ TicketHunt/PregledListeDogadjaja.h
@@ -0,0 +1,18 @@
+#ifndef PREGLED_LISTE_DOGADJAJA_H
+#define PREGLED_LISTE_DOGADJAJA_H
+#include"KreiranjeDogadjaja.h"
+typedef struct cvor{
+	char naziv[MAX];
+	struct Datum datum;
+	char mjesto[MAX];
+	char ID[MAX];
+	struct Vrijeme_odrzavanja vrijeme;
+	int cijena_ulaznice;
+	int broj_mjesta;
+	int broj_prodatih_ulaznica;
+	int kupuje_na_ime;
+	char opis_dogadjaja[MAX];
+	struct cvor *sljedeci;
+} CVOR;
+void pregledListeDogadjaja(char *file);
+#endif // PREGLED_LISTE_DOGADJAJA_H
