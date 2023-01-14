f89e55415ae3cc70661edc899468787b13fad544
mihajloseva <mihajlo.seva@student.etf.unibl.org>
2023-01-14 13:17:17 +0100

    Pregled informacija i opisa dogadjaja - dodat opis dogadjaja

--- TicketHunt/PregledDogadjaja.c
+++ TicketHunt/PregledDogadjaja.c
@@ -3,7 +3,8 @@
 
 void PregledDogadjaja()
 {
-    FILE* fp;
+    FILE *fp, *dat;
+    
     if (fp = fopen("Dogadjaji.txt", "r"))
     {
         DOGADJAJ temp;
@@ -11,11 +12,26 @@ void PregledDogadjaja()
         while (  fscanf(fp, "%s %d.%d.%d %d:%d %s %d %d %d %d %s\n", temp.naziv, &temp.datum.dan,
                  &temp.datum.mjesec, &temp.datum.godina, &temp.vrijeme.sat, &temp.vrijeme.minut, temp.mjesto,
                  &temp.cijena_ulaznice, &temp.broj_mjesta, &temp.broj_prodatih_ulaznica, &temp.kupuje_na_ime, temp.ID)   !=  EOF )
-
-            printf("%s %d.%d.%d %d:%d %s %d %d %d %d %s\n", temp.naziv, temp.datum.dan,
-                temp.datum.mjesec, temp.datum.godina, temp.vrijeme.sat, temp.vrijeme.minut, temp.mjesto,
-                temp.cijena_ulaznice, temp.broj_mjesta, temp.broj_prodatih_ulaznica, temp.kupuje_na_ime, temp.ID);
-
+            {
+                printf("%s %d.%d.%d %d:%d %s %d %d %d %d %s\n", temp.naziv, temp.datum.dan,
+                    temp.datum.mjesec, temp.datum.godina, temp.vrijeme.sat, temp.vrijeme.minut, temp.mjesto,
+                    temp.cijena_ulaznice, temp.broj_mjesta, temp.broj_prodatih_ulaznica, temp.kupuje_na_ime, temp.ID);
+                    
+                    char pomocno_ime[MAX];
+                    char ekstenzija[6]=".txt";
+                    strcpy(pomocno_ime, temp.naziv);
+                    strcat(pomocno_ime,ekstenzija);
+                    char red[200];
+                if (dat=fopen(pomocno_ime,"r") != NULL)
+                {
+                    while ( fgets(red,200,dat) != NULL)
+                    {
+                        printf("%s",red);
+                    }
+                    fclose(dat);
+                }
+                printf("\n");
+            }
         fclose(fp);
     }
     else
