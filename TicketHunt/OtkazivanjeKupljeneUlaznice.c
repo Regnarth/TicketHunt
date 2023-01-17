fb4e18a87b725591fe8f78f9743e4df8d82c0abf
Marija Zjajic <120033001+MarijaZjajic@users.noreply.github.com>
2023-01-17 18:57:59 +0100

    Dodano brisanje i iz datoteke Ulaznice

--- TicketHunt/OtkazivanjeKupljeneUlaznice.c
+++ TicketHunt/OtkazivanjeKupljeneUlaznice.c
@@ -1,41 +1,42 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-//#include "KupovinaUlaznice.c"
-//#include "KreiranjeDogadjaja.h"
+#include "KupovinaUlaznice.c"
+#include "KupovinaUlaznice.h"
+#include "Prijava.h"
+#include "Prijava.c"
+#include "Registracija.h"
 
-
-void otkazivanjeKupljeneUlaznice(char* korisnickoIme)
+void otkazivanjeKupljeneUlaznice(USER* user)
 {
     FILE* f;
-    char* sifra_ulaznice;
+    char sifra_ulaznice[255];
     int broj_linije=0;
+    int ukupan_broj_linija = 0;
     printf("Unesite sifru ulaznice koju zelite da otkazete: ");
     scanf("%s", sifra_ulaznice);
     int sifra_validna=0;
     if(f=fopen("SifraKorisnicko.txt", "r"))
     {
+
         int lineLength = 255;
         char line[lineLength];
-
-
         while(fgets(line, lineLength, f))
     {
-
+        ukupan_broj_linija++;
         char* pch;
         pch = strtok (line," ");
         char* sifra = pch;
         if(strcmp(sifra,sifra_ulaznice)==0)
         {
             pch = strtok (NULL, " ");
-            char* userName = pch;
-            if(strcmp(userName, korisnickoIme)==0)
+            char* korisnickoime = pch;
+            if(strcmp(korisnickoime, user->username)==0)
             {
                 sifra_validna=1;
+                broj_linije = ukupan_broj_linija;
             }
         }
-        broj_linije++;
-
     }
     fclose(f);
     if(sifra_validna==0)
@@ -44,7 +45,12 @@ void otkazivanjeKupljeneUlaznice(char* korisnickoIme)
     }
     else if (sifra_validna == 1){
         obrisiLiniju(broj_linije);
+        obrisiLiniju_drugifajl(broj_linije);
+         printf("Ulaznica je uspjesno otkazana.");
     }
+}
+else{
+    printf("Greska prilikom otvaranja fajla!");
 }
 }
 
@@ -70,7 +76,29 @@ void obrisiLiniju(int broj_linije){
     remove(filename);
     rename("replica.txt", filename);
     remove(pomocnifajl);
-
-    printf("Ulaznica je uspjesno otkazana.");
 }
 
+void obrisiLiniju_drugifajl(int broj_linije)
+{
+    int line_number=0;
+    FILE* fptr1 = fopen("Ulaznice.txt","r");
+    FILE* fptr2 = fopen("replica.txt", "w");
+    char curr = getc(fptr1);
+    if(curr!=EOF) {line_number =1;}
+    while(1)
+    {
+      if(broj_linije!= line_number)
+        putc(curr, fptr2);
+        curr = getc(fptr1);
+        if(curr =='\n') line_number++;
+        if(curr == EOF) break;
+    }
+    fclose(fptr1);
+    fclose(fptr2);
+    const char* filename="Ulaznice.txt";
+    const char* pomocnifajl="pomocni.txt";
+    rename(filename, pomocnifajl);
+    remove(filename);
+    rename("replica.txt", filename);
+    remove(pomocnifajl);
+}
