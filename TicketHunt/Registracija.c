9a81255e61da59d0c6fe69a33381942c0c272b54
David Đenadija <59781575+Regnarth@users.noreply.github.com>
2023-01-21 10:49:45 +0100

    Update Registracija.c

--- TicketHunt/Registracija.c
+++ TicketHunt/Registracija.c
@@ -7,19 +7,19 @@ void register_user()
     USER user;
     FILE* fp;
     printf("Enter a username: ");
-    scanf("%s",user.username);
-    if(!provjeriUsername(user.username))
+    scanf("%s", user.username);
+    if (!provjeriUsername(user.username))
     {
-        if(fp=fopen("Korisnici.txt","a+"))
+        if (fp = fopen("Korisnici.txt", "a+"))
         {
 
-            printf("Enter your name: ");
-            scanf("%s",user.ime);
-            printf("Enter your lastname: ");
-            scanf("%s",user.prezime);
-            printf("Enter a password: ");
-            scanf("%s",user.password);
-            fprintf(fp,"%s %s %s %s\n",user.ime,user.prezime,user.username,user.password);
+            printf("Unesite vase ime: ");
+            scanf("%s", user.ime);
+            printf("Unesite vase prezime: ");
+            scanf("%s", user.prezime);
+            printf("Unesite sifru: ");
+            scanf("%s", user.password);
+            fprintf(fp, "%s %s %s %s\n", user.ime, user.prezime, user.username, user.password);
             fclose(fp);
         }
         else printf("Neuspjesno otvoren fajl!");
@@ -31,31 +31,34 @@ void register_user()
 
 int provjeriUsername(char* username)
 {
-    int size=1;
+    int size = 1;
     USER pom;
     FILE* fp;
-    int i=0;
-    if(fp=fopen("Korisnici.txt","r"))
+    int i = 0;
+    if (fp = fopen("Korisnici.txt", "r"))
     {
-        while(fscanf(fp,"%s %s %s %s",pom.ime,pom.prezime,pom.username,pom.password)!=EOF)
+        while (fscanf(fp, "%s %s %s %s", pom.ime, pom.prezime, pom.username, pom.password) != EOF)
         {
-            if(strcmp(username,pom.username)==0)return 1;
+            if (strcmp(username, pom.username) == 0) {
+                fclose(fp);
+                return 1;
+            }
         }
         fclose(fp);
     }
-     if(fp=fopen("Administratori.txt","r"))
+    if (fp = fopen("Administratori.txt", "r"))
     {
-        while(fscanf(fp,"%s %s %s %s",pom.ime,pom.prezime,pom.username,pom.password)!=EOF)
+        while (fscanf(fp, "%s %s %s %s", pom.ime, pom.prezime, pom.username, pom.password) != EOF)
         {
-            if(strcmp(username,pom.username)==0)return 1;
+            if (strcmp(username, pom.username) == 0)return 1;
         }
         fclose(fp);
     }
-     if(fp=fopen("Klijenti.txt","r"))
+    if (fp = fopen("Klijenti.txt", "r"))
     {
-        while(fscanf(fp,"%s %s %s %s",pom.ime,pom.prezime,pom.username,pom.password)!=EOF)
+        while (fscanf(fp, "%s %s %s %s", pom.ime, pom.prezime, pom.username, pom.password) != EOF)
         {
-            if(strcmp(username,pom.username)==0)return 1;
+            if (strcmp(username, pom.username) == 0)return 1;
         }
         fclose(fp);
     }
