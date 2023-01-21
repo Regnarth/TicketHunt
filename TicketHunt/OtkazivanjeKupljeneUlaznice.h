1e2504284c39616bc4e84b9ed305fb3046968f2a
Marija Zjajic <120033001+MarijaZjajic@users.noreply.github.com>
2023-01-21 10:56:07 +0100

    Upload OtkazivanjeKupljeneUlaznice.h

---
+++ TicketHunt/OtkazivanjeKupljeneUlaznice.h
@@ -0,0 +1,12 @@
+#ifndef OTKAZIVANJE_KUPLJENE_ULAZNICE_H
+#define OTKAZIVANJE_KUPLJENE_ULAZNICE_H
+#define MAX 20
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+void otkazivanjeKupljeneUlaznice(char* korisnickoIme);
+void obrisiLiniju(int broj_linije);
+void brisanjeLinije_drugifajl(int broj_linije);
+#endif
