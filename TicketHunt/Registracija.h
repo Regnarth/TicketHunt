3ef69b55068320887ff5ac117b79db49d0b8c08c
David Đenadija <59781575+Regnarth@users.noreply.github.com>
2023-01-21 10:49:15 +0100

    Update Registracija.h

--- TicketHunt/Registracija.h
+++ TicketHunt/Registracija.h
@@ -1,6 +1,6 @@
 #ifndef REGISTRACIJA_H
 #define REGISTRACIJA_H
- // REGISTRACIJA_H
+// REGISTRACIJA_H
 
 #define MAX_USERNAME_LENGTH 50
 #define MAX_PASSWORD_LENGTH 50
@@ -8,9 +8,10 @@
 struct User {
     char ime[MAX_USERNAME_LENGTH];
     char prezime[MAX_USERNAME_LENGTH];
-  char username[MAX_USERNAME_LENGTH];
-  char password[MAX_PASSWORD_LENGTH];
- int kredit;
+    char username[MAX_USERNAME_LENGTH];
+    char password[MAX_PASSWORD_LENGTH];
+    int kredit;
+    int vrstaNaloga;
 };
 typedef struct User USER;
 
